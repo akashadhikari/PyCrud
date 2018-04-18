7f5c1726cd8665355d9b9b8a6cd383f2beebb2ce
Akash Adhikari <akash_532998@yahoo.com>
2018-04-18 17:14:19 +0545

    Partial sort operation

--- src/sqlite.py
+++ src/sqlite.py
@@ -110,4 +110,34 @@ remove = remove_emp(emp2)
 
 print(show_all())
 
+
+def insertion_sort(alist):
+    for index in range(1, len(alist)):
+
+        # We'll first track the current positional value
+        current_value = alist[index]
+
+        # We just checked if the index of current position is greater than 0 AND
+        # previous positional value is  greater than current
+        while index > 0 and alist[index - 1] > current_value:
+            # if thats the case, sort them
+            alist[index] = alist[index - 1]
+
+            # We just deducted the index by 1
+            index = index - 1
+
+        # set the changed index value to current
+        alist[index] = current_value
+
+
+# alist = [5, 2, 4, 6, 1, 3]
+
+alist = list(show_all())
+
+insertion_sort(alist)
+
+print("***********After Sorting***********")
+
+print(alist)
+
 conn.close()
