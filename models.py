e9ee3efedf3d42b3d0d896be1f7b5084524e2f20
Akash Adhikari <akash_532998@yahoo.com>
2018-04-18 09:28:28 +0545

    Add extra fields

--- models.py
+++ models.py
@@ -2,10 +2,18 @@ class Aayulogic:
 
     # We pass in some information of Aayulogic Employee
 
-    def __init__(self, first, last, salary):
+    def __init__(self, first, last, salary, phone, bio, birth_date, gender, longitude, latitude, social_media):  # Image
         self.first = first
         self.last = last
         self.salary = salary
+        self.phone = phone
+        self.bio = bio
+        self.birth_date = birth_date
+        self.gender = gender
+        self.longitude = longitude
+        self.latitude = latitude
+        self.social_media = social_media
+        # Add image
 
     @property
     def email(self):
--- sqlite.py
+++ sqlite.py
@@ -7,19 +7,36 @@ conn = sqlite3.connect(':memory:')  # :memory: or db name
 c = conn.cursor()  # cursor object to fetch results
 
 # We know what we are doing here, right? :D
-
 c.execute("""CREATE TABLE employees (
              first text,
              last text,
-             salary integer
+             salary decimal,
+             phone varchar(30),
+             bio text,
+             birth_date varchar,
+             gender text,
+             longitude decimal,
+             latitude decimal,
+             social_media varchar
              )""")
 
 
 # Some custom CRUD functions using native SQLite commands
 def insert_emp(emp):
     with conn:
-        c.execute("INSERT INTO employees VALUES (:first, :last, :salary) ",
-              {'first': emp.first, 'last': emp.last, 'salary': emp.salary})
+        c.execute("INSERT INTO employees VALUES (:first, :last, :salary, :phone, :bio, :birth_date, :gender, :longitude,"
+                  " :latitude, :social_media) ",
+              {'first': emp.first,
+               'last': emp.last,
+               'salary': emp.salary,
+               'phone': emp.phone,
+               'bio': emp.bio,
+               'birth_date': emp.birth_date,
+               'gender': emp.gender,
+               'longitude': emp.longitude,
+               'latitude': emp.latitude,
+               'social_media': emp.social_media
+               })
 
 
 def get_emps_by_name(lastname):
@@ -39,10 +56,15 @@ def remove_emp(emp):
         c.execute("DELETE from employees WHERE first=:first AND last=:last",
                   {'first': emp.first, 'last': emp.last})
 
+"""
+:first, :last, :salary, :phone, :bio, :birth_date, :gender, :longitude,"
+                  " :latitude, :social_media
+"""
 
 # Testing it on arbitrary data
-emp1 = Aayulogic('John', 'Doe', 80000)
-emp2 = Aayulogic('Ram', 'Doe', 111111)
+emp1 = Aayulogic('John', 'Doe', 80000.002, '984585485', 'Hello All!', '1996/2/3', 'Male', 27.29, 32.53, 'fb.com/emp1')
+emp2 = Aayulogic('Ram', 'Doe', 111111.25, '999999999', 'Heyyy', '1990/1/1', 'Male', 27.2099, 32.5875453, 'fb.com/emp2')
+
 
 insert_emp(emp1)
 insert_emp(emp2)
