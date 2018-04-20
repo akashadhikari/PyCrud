44eec2e62d9fa963f893fdff572308e541cd8547
Akash Adhikari <akash_532998@yahoo.com>
2018-04-20 13:26:23 +0545

    Binary sort- unoptimized code

--- src/postgres.py
+++ src/postgres.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import psycopg2
 
 
@@ -8,7 +9,6 @@ class PostgresConnect:
 
 class CRUDPostgres:
 
-    @staticmethod
     def create():
         print("::::::::::::::WELCOME TO CRAPPY DATABASE SOFTWARE::::::::::::::")
         id = input("Enter your ID: ")
@@ -28,15 +28,21 @@ class CRUDPostgres:
                   "boolean, address, url, image_url) VALUES (%s, %s, %s, %s, %s, %s,%s, %s, %s,%s, %s)",
                   (id, first_name, last_name, email, phone_number, text, date, boolean,
                    address, url, image_url))
+        print("Successfully inserted! :)")
 
-    @staticmethod
-    def read():
+    def read_from_id():
         # Read
-        PostgresConnect.c.execute("SELECT id, first_name, email from custom WHERE id>99999")
+        id_read = input("Enter the ID to view its information")
+        PostgresConnect.c.execute("SELECT id, first_name, last_name, email from custom WHERE id=%s",
+                                  (id_read,))
         rows = PostgresConnect.c.fetchall()
         print(rows)
 
-    @staticmethod
+    def read_all():
+        PostgresConnect.c.execute("SELECT * from custom")
+        rows = PostgresConnect.c.fetchall()
+        print(rows)
+
     def update():
         print("Update a record")
         upd_id = input("Enter your ID to update: ")
@@ -44,39 +50,89 @@ class CRUDPostgres:
         PostgresConnect.c.execute("UPDATE custom SET email=%s WHERE id=%s", (upd_email, upd_id))
         print("Successfully updated")
 
-    @staticmethod
     def delete():
         # Delete
         print("Delete a record")
-        PostgresConnect.c.execute("DELETE FROM custom WHERE id=989886594")
+        del_id = input("Enter the ID to be deleted")
+        PostgresConnect.c.execute("DELETE FROM custom WHERE id=%s", (del_id,))
 
 
-CRUDPostgres.read()
+PostgresConnect.c.execute("SELECT * from custom")
+rows = PostgresConnect.c.fetchall()
+#print(rows)
+
+li = []
+for row in rows:
+    li.append(row)
+#print(li[0:5])
+
+temp = []
+for l in li:
+    x = list(l)
+    temp.append(x)
+#print(temp)
+
+array = []
+for each in temp:
+    array.append(each[0])
+print(type(array))
+#print(array)
+
+class Sort:
+
+    def insertion_sort(array):
+        for index in range(1, len(array)):
+
+            # We'll first track the current positional value
+            current_value = array[index]
+
+            # We just checked if the index of current position is greater than 0 AND
+            # previous positional value is  greater than current
+            while index > 0 and array[index - 1] > current_value:
+                # if thats the case, sort them
+                array[index] = array[index - 1]
+
+                # We just deducted the index by 1
+                index = index - 1
+
+            # set the changed index value to current
+            array[index] = current_value
+
+    # What if I get a better search algorithm? I'll simply bind it over here.
+
+    def another_better_search_algorithm(array):
+        # How about implementing Count sort or something like that?
+        pass
+
+Sort.insertion_sort(array)
+print(array)
+
+anum = 23323232                   # number to search for
+#array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]  # get some random numbers
+print(anum, array)          # show us what you've got
+
+#    Search for number in array
+def binary_search(number, array, lo, hi):
+
+    if hi < lo: return -1       # no more numbers
+    mid = (lo + hi) // 2        # midpoint in array
+    if number == array[mid]:
+        return mid                  # number found here
+    elif number < array[mid]:
+        return binary_search(number, array, lo, mid - 1)     # try left of here
+    else:
+        return binary_search(number, array, mid + 1, hi)     # try above here
+
+def my_search(anum, array):     # convenience interface to binary_search()
+    return binary_search(anum, array, 0, len(array) - 1)
+
+pos = my_search(anum, array)
+if pos < 0:
+    print("not found")
+else:
+    print("found at position", pos)
 
 
-# rows = PostgresConnect.c.fetchall()
-# print(rows)
-#
-# # Sorting
-# temp = []
-#
-# # Tuple to list conversion for convenience
-# for tup in rows:
-#     temp.append(list(tup))
-# print(temp)
-#
-#
-# a_list = []
-#
-# # Appending the unordered list
-# for each in temp:
-#     a_list.append(each[0])
-#
-# Sort.insertion_sort(a_list)
-#
-# print("***********After Sorting***********")
-#
-# # print(a_list)
 
 PostgresConnect.conn.commit()
 PostgresConnect.c.close()
